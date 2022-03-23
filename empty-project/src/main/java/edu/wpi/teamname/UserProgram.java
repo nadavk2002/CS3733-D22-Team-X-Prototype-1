e93a8078ac02e14155b0652c6b3bbbdab3d56ef7
Darren Kwee <darrenjk75@gmail.com>
2022-03-23 16:12:28 -0400

    added create table statement for location

--- empty-project/src/main/java/edu/wpi/teamname/UserProgram.java
+++ empty-project/src/main/java/edu/wpi/teamname/UserProgram.java
@@ -2,63 +2,57 @@ package edu.wpi.teamname;
 
 import java.util.Scanner;
 
-public class UserProgram
-{
+public class UserProgram {
 
-    /**
-     * Starts the program that the user will interact with.
-     * This method runs until the user cannot login or they enter "6" to exit the program
-     * @param username used to login
-     * @param password used to login
-     */
-    public static void executeProgram(String username, String password)
-    {
-        if(username.equals("admin") && password.equals("admin"))
-        {
-            System.out.println("Successfully logged in!");
-        }
-        else
-        {
-            System.out.println("Incorrect username or password");
-            return;
-        }
-
-        System.out.println("1 – Location Information\n" +
-                           "2 – Change Floor and Type\n" +
-                           "3 – Enter Location\n" +
-                           "4 – Delete Location\n" +
-                           "5 – Save Locations to CSV file\n" +
-                           "6 – Exit Program");
-
-        //This loop will end when the user selects option 6
-        while(true)
-        {
-            Scanner input = new Scanner(System.in);
-            String option = input.nextLine();
-            switch (option) {
-                case ("1"):
-                    System.out.println("option 1 placeholder");
-                    break;
-                case ("2"):
-                    System.out.println("option 2 placeholder");
-                    break;
-                case ("3"):
-                    System.out.println("option 3 placeholder");
-                    break;
-                case ("4"):
-                    System.out.println("option 4 placeholder");
-                    break;
-                case ("5"):
-                    System.out.println("option 5 placeholder");
-                    break;
-                case ("6"):
-                    return;
-                default:
-                    System.out.println("Your input should be one number between 1 and 6, try again");
-                    break;
-            }
-        }
-
+  /**
+   * Starts the program that the user will interact with. This method runs until the user cannot
+   * login or they enter "6" to exit the program
+   *
+   * @param username used to login
+   * @param password used to login
+   */
+  public static void executeProgram(String username, String password) {
+    if (username.equals("admin") && password.equals("admin")) {
+      System.out.println("Successfully logged in!");
+    } else {
+      System.out.println("Incorrect username or password");
+      return;
     }
 
+    System.out.println(
+        "1 – Location Information\n"
+            + "2 – Change Floor and Type\n"
+            + "3 – Enter Location\n"
+            + "4 – Delete Location\n"
+            + "5 – Save Locations to CSV file\n"
+            + "6 – Exit Program");
+
+    // This loop will end when the user selects option 6
+    while (true) {
+      Scanner input = new Scanner(System.in);
+      String option = input.nextLine();
+      switch (option) {
+        case ("1"):
+          System.out.println("option 1 placeholder");
+          break;
+        case ("2"):
+          System.out.println("option 2 placeholder");
+          break;
+        case ("3"):
+          System.out.println("option 3 placeholder");
+          break;
+        case ("4"):
+          System.out.println("option 4 placeholder");
+          break;
+        case ("5"):
+          System.out.println("option 5 placeholder");
+          break;
+        case ("6"):
+          return;
+        default:
+          System.out.println("Your input should be one number between 1 and 6, try again");
+          break;
+      }
+    }
+  }
 }
--- empty-project/src/main/java/edu/wpi/teamname/Xdb.java
+++ empty-project/src/main/java/edu/wpi/teamname/Xdb.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 /** Created by Darren Kwee */
 public class Xdb {
-
   public static void main(String[] args) {
     System.out.println("-----Testing Apache Derby Embedded Connection-----");
     // checks whether the driver is working
@@ -21,7 +20,7 @@ public class Xdb {
     Connection connection = null;
     try {
       connection = DriverManager.getConnection("jdbc:derby:Xdb;create=true");
-      Statement testStatement = connection.createStatement();
+      // Statement testStatement = connection.createStatement();
       //      testStatement.execute("CREATE TABLE TEST(ID INT PRIMARY KEY)");
       //      testStatement.execute("INSERT INTO TEST VALUES(3)");
     } catch (SQLException e) {
@@ -31,14 +30,28 @@ public class Xdb {
     }
     System.out.println("Apache Derby connection established :D");
 
-    //Ensures username and password are passed as arguments
-    if(args.length < 2 || args[0] == null || args[1] == null)
-    {
-      System.out.println("Enter username and password as arguments I.E. \"Xdb username password\"");
+    // creates tables
+    try {
+      Statement initialization = connection.createStatement();
+      initialization.execute(
+          "CREATE TABLE Location(nodeID CHAR(10) PRIMARY KEY NOT NULL, "
+              + "xCoord INT NOT NULL, yCoord INT NOT NULL, "
+              + "floor VARCHAR(2) NOT NULL, "
+              + "building VARCHAR(10) NOT NULL, "
+              + "nodeType CHAR(4) NOT NULL, "
+              + "longName VARCHAR(50) NOT NULL, shortName VARCHAR(30))");
+    } catch (SQLException e) {
+      System.out.println("Table creation failed. Check output console.");
+      e.printStackTrace();
       return;
     }
-    else
-    {
+    System.out.println("Database created successfully");
+
+    // Ensures username and password are passed as arguments
+    if (args.length < 2 || args[0] == null || args[1] == null) {
+      System.out.println("Enter username and password as arguments I.E. \"Xdb username password\"");
+      return;
+    } else {
       UserProgram.executeProgram(args[0], args[1]);
     }
   }
