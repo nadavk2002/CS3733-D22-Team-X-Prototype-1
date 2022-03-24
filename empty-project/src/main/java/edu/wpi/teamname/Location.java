5fcb8d50de92b43b378918958e2a19374ec2962c
Darren Kwee <darrenjk75@gmail.com>
2022-03-24 18:45:18 -0400

    added another constructor with only nodeID as parameter

--- empty-project/src/main/java/edu/wpi/teamname/Location.java
+++ empty-project/src/main/java/edu/wpi/teamname/Location.java
@@ -1,5 +1,7 @@
 package edu.wpi.teamname;
 
+import java.util.Objects;
+
 public class Location {
   private String nodeID;
   private int xCoord, yCoord;
@@ -17,6 +19,17 @@ public class Location {
     nodeType = null;
     longName = null;
     shortName = null;
+  }
+
+  public Location(String nodeID) {
+    this.nodeID = nodeID;
+    this.xCoord = -1;
+    this.yCoord = -1;
+    this.floor = null;
+    this.building = null;
+    this.nodeType = null;
+    this.longName = null;
+    this.shortName = null;
   }
 
   public Location(
@@ -100,5 +113,18 @@ public class Location {
 
   public String getShortName() {
     return shortName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Location location = (Location) o;
+    return Objects.equals(nodeID, location.nodeID);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nodeID);
   }
 }
