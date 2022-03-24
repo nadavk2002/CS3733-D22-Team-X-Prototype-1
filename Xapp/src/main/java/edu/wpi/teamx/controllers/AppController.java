061f7c0235ee2e41b504f66d940f85f3e0aa51d5
Sharonronron <slwu@wpi.edu>
2022-03-24 17:58:57 -0400

    initial commit for medicine delivery scene

--- Xapp/src/main/java/edu/wpi/teamx/controllers/AppController.java
+++ Xapp/src/main/java/edu/wpi/teamx/controllers/AppController.java
@@ -9,10 +9,18 @@ import javafx.scene.control.Button;
 
 public class AppController {
   @FXML private Button ReqLangButton;
+  @FXML private Button ReqMedicineDelivery;
 
   @FXML
   void ReqLangButton() throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamx/views/ReqLang.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
+  @FXML
+  void ReqMedicineDelivery() throws IOException {
+    Parent root =
+        FXMLLoader.load(getClass().getResource("/edu/wpi/teamx/views/Medicine_Delivery.fxml"));
     App.getPrimaryStage().getScene().setRoot(root);
   }
 }
