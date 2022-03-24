b36f037802990fd065d98fd7db081948f5b2642b
jrwecler <jrwecler@wpi.edu>
2022-03-24 18:01:56 -0400

    Internal Transport scene built with scene switching working

--- Xapp/src/main/java/edu/wpi/teamx/controllers/ReqInTransportController.java
+++ Xapp/src/main/java/edu/wpi/teamx/controllers/ReqInTransportController.java
@@ -2,15 +2,25 @@ package edu.wpi.teamx.controllers;
 
 import edu.wpi.teamx.App;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
 
-public class ReqInTransportController {
+public class ReqInTransportController implements Initializable {
   @FXML private Button AppButton;
+  @FXML private ChoiceBox<String> transportLocation;
+  @FXML private ChoiceBox<String> addAccommodations;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    transportLocation.getItems().addAll(new String[] {"X-Ray Lab", "ICU", "Lobby", "Morgue"});
+    addAccommodations.getItems().addAll(new String[] {"Wheelchair", "Bed", "Ventilation", "Nurse"});
+  }
 
   @FXML
   void AppButton() throws IOException {
