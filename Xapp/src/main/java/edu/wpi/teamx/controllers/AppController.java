415e92a748c9ee29d26fa060c273fa397236844c
Dylan Nguyen <dnguyen2065@gmail.com>
2022-03-24 16:58:43 -0400

    Completed Scene drop down choices are populated

--- Xapp/src/main/java/edu/wpi/teamx/controllers/AppController.java
+++ Xapp/src/main/java/edu/wpi/teamx/controllers/AppController.java
@@ -9,10 +9,17 @@ import javafx.scene.control.Button;
 
 public class AppController {
   @FXML private Button ReqLangButton;
+  @FXML private Button mealReqButton;
 
   @FXML
   void ReqLangButton() throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamx/views/ReqLang.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
+  @FXML
+  void mealReqButton() throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamx/views/mealRequest.fxml"));
     App.getPrimaryStage().getScene().setRoot(root);
   }
 }
