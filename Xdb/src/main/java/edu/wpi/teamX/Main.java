cc8cf363981bef38fcff0c2ee5b6a2ec79e70fb3
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2022-03-25 20:20:12 -0400

    deleted past Main.java and App.java, renamed Xdb.java to Main.java

--- Xdb/src/main/java/edu/wpi/teamX/Main.java
+++ Xdb/src/main/java/edu/wpi/teamX/Main.java
@@ -1,8 +1,49 @@
 package edu.wpi.teamX;
 
-public class Main {
+import java.sql.*;
 
+public class Main {
   public static void main(String[] args) {
-    App.launch(App.class, args);
+    System.out.println("-----Testing Apache Derby Embedded Connection-----");
+    // checks whether the driver is working
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Apache Derby Driver not found.");
+      e.printStackTrace();
+      return;
+    }
+    System.out.println("Apache Derby driver registered!");
+
+    // tries to create the database and establish a connection
+    Connection connection = null;
+    try {
+      connection =
+          DriverManager.getConnection("jdbc:derby:spike_b_db;create=true", "admin", "admin");
+    } catch (SQLException e) {
+      System.out.println("Connection failed. Check output console.");
+      e.printStackTrace();
+      return;
+    }
+    System.out.println("Apache Derby connection established :D");
+
+    // creates table
+    try {
+      Statement initialization = connection.createStatement();
+      initialization.execute(
+          "CREATE TABLE Location(nodeID CHAR(10) PRIMARY KEY NOT NULL, "
+              + "xCoord INT, yCoord INT, "
+              + "floor VARCHAR(2), "
+              + "building VARCHAR(10), "
+              + "nodeType CHAR(4), "
+              + "longName VARCHAR(50), shortName VARCHAR(30))");
+    } catch (SQLException e) {
+      System.out.println("Table creation failed. Check output console.");
+      e.printStackTrace();
+      return;
+    }
+    System.out.println("Database created successfully");
+
+    UserProgram.executeProgram(connection);
   }
 }
