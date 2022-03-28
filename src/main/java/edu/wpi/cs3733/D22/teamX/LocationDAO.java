85f747d200b0af41922e05fd4692e01902f41a7f
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-03-27 22:11:04 -0400

    created locationDAO interface

---
+++ src/main/java/edu/wpi/cs3733/D22/teamX/LocationDAO.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs3733.D22.teamX;
+
+
+import java.util.LinkedList;
+
+public interface LocationDAO {
+    public LinkedList<Location> getAllLocations();//gets all
+    public Location getLocation (String nodeID); //gets induviduvidual
+    public void updateLocation (Location location); //updates induvidual location
+    public void deleteLocation (Location location); //removes induvidual location
+}
