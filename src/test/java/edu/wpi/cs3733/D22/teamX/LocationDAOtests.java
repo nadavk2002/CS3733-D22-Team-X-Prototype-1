e81d2eeaf58cb07dbdc4339e44879be0b8acf06d
salochinYom <nrmoy2@gmail.com>
2022-03-28 00:06:42 -0400

    started making tests
    how do i start the db?

--- src/test/java/edu/wpi/cs3733/D22/teamX/LocationDAOtests.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/LocationDAOtests.java
@@ -1,6 +1,18 @@
 package edu.wpi.cs3733.D22.teamX;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LocationDAOtests {
+
+    //connection varible
+
+    @BeforeEach
+    public void setupDB(){
+        //initialize Xdb
+
+    }
+
+    //test
+
 }
