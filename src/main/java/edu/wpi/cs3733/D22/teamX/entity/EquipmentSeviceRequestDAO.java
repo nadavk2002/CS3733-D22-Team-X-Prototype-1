5685a969a450d5bfbf5c9c22d623b9a39ae9c46e
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-03-28 19:21:33 -0400

    added EquipmentServiceRequestDAO interface

---
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/EquipmentSeviceRequestDAO.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs3733.D22.teamX.entity;
+
+import java.util.List;
+
+public interface EquipmentSeviceRequestDAO {
+    public List<EquipmentServiceRequest> getAllEquipmentServiceRequests();
+    public EquipmentServiceRequest getEquipmentServiceRequest(); //add ids
+    public void deleteEquipmentServiceRequest(EquipmentServiceRequest equipmentServiceRequest);
+    public void updateEquipmentServiceRequest(EquipmentServiceRequest equipmentServiceRequest);
+}
