3afe1215df619cae5e9dd06fb6ba700560e820cb
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-03-29 18:35:40 -0400

    started writing DAO test

--- src/test/java/edu/wpi/cs3733/D22/teamX/LocationDAOtests.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/LocationDAOtests.java
@@ -2,8 +2,10 @@ package edu.wpi.cs3733.D22.teamX;
 
 import java.sql.Connection;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class LocationDAOtests {
+  // based on the CSV file as of 3/29/22
 
   // connection varible
   Connection connection;
@@ -13,6 +15,9 @@ public class LocationDAOtests {
     // initialize Xdb
     connection = Xdb.initializeDB();
   }
+
+  @Test
+  public void TestLocationDAO() {}
 
   // test
 
