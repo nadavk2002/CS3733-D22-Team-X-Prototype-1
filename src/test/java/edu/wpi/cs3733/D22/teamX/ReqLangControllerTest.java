4c440f137a91b2e47258f84740819581e5780622
Dylan Nguyen <dnguyen2065@gmail.com>
2022-03-30 21:34:39 -0400

    Added javaFX tests for all relevant controllers

---
+++ src/test/java/edu/wpi/cs3733/D22/teamX/ReqLangControllerTest.java
@@ -0,0 +1,55 @@
+package edu.wpi.cs3733.D22.teamX;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
+
+import java.io.IOException;
+import java.util.Objects;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class ReqLangControllerTest extends ApplicationTest {
+  @Override
+  public void start(Stage stage) throws IOException {
+    Parent root =
+        FXMLLoader.load(
+            Objects.requireNonNull(
+                getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/ReqLang.fxml")));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Test
+  public void testRoomNumTextField() {
+    clickOn("#roomNum");
+    write("h");
+    verifyThat("#roomNum", hasText("h"));
+  }
+
+  @Test
+  public void testAssignStaffTextField() {
+    clickOn("#assignStaff");
+    write("h");
+    verifyThat("#assignStaff", hasText("h"));
+  }
+
+  @Test
+  public void testServiceStatusTextField() {
+    clickOn("#serviceStatus");
+    write("h");
+    verifyThat("#serviceStatus", hasText("h"));
+  }
+
+  @Test
+  public void testResetButton() {
+    clickOn("#resetFields");
+    verifyThat("#roomNum", hasText(""));
+    verifyThat("#assignStaff", hasText(""));
+    verifyThat("#serviceStatus", hasText(""));
+  }
+}
