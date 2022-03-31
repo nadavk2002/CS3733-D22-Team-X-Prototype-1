2aec3da90d7f8c6058804dfa9ff31262b2773699
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-03-30 20:27:18 -0400

    updated build.gradle and commented out loadcsv

--- src/test/java/edu/wpi/cs3733/D22/teamX/LoadCSVTest.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/LoadCSVTest.java
@@ -4,21 +4,17 @@
 
 package edu.wpi.cs3733.D22.teamX;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import org.junit.jupiter.api.Test;
-
 public class LoadCSVTest {
 
-  @Test
-  public void testLoadLocationCSV() {
-    Connection connection = Xdb.initializeDB();
-    try {
-      connection.close();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
+  //  @Test
+  //  public void testLoadLocationCSV() {
+  //    Connection connection = Xdb.initializeDB();
+  //    try {
+  //      connection.close();
+  //    } catch (SQLException e) {
+  //      e.printStackTrace();
+  //    }
+  //  }
 
   //  @Test
   //  public void testSaveCSV() {
