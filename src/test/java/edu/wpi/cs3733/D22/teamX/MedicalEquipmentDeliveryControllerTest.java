c233b0e4a7e1902a50788e29597154b9ff15882f
Dylan Nguyen <dnguyen2065@gmail.com>
2022-03-31 19:27:46 -0400

    Fixed Junit tests for medicineDelivery and MedicalEquipDelivery and fixed naming for tests

--- src/test/java/edu/wpi/cs3733/D22/teamX/MedicalEquipmentDeliveryControllerTest.java
+++
@@ -1,3 +0,0 @@
-package edu.wpi.cs3733.D22.teamX;
-
-public class MedicalEquipmentDeliveryControllerTest {}
--- src/test/java/edu/wpi/cs3733/D22/teamX/MedicineDeliveryControllerTest.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/MedicineDeliveryControllerTest.java
@@ -18,22 +18,54 @@ public class MedicineDeliveryControllerTest extends ApplicationTest {
     Parent root =
         FXMLLoader.load(
             Objects.requireNonNull(
-                getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/equipmentDelivery.fxml")));
+                getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/Medicine_Delivery.fxml")));
     Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
   }
+  //
+  @Test
+  public void testPatientNameTextField() {
+    clickOn("#patientName");
+    write("h");
+    verifyThat("#patientName", hasText("h"));
+  }
 
   @Test
-  public void testAmountTextField() {
-    clickOn("#amountField");
+  public void testRXNumTextField() {
+    clickOn("#rxNum");
     write("h");
-    verifyThat("#amountField", hasText("h"));
+    verifyThat("#rxNum", hasText("h"));
+  }
+
+  @Test
+  public void testRoomNumTextField() {
+    clickOn("#roomNum");
+    write("h");
+    verifyThat("#roomNum", hasText("h"));
+  }
+
+  @Test
+  public void testAssignStaffTextField() {
+    clickOn("#assignStaff");
+    write("h");
+    verifyThat("#assignStaff", hasText("h"));
+  }
+
+  @Test
+  public void testServiceStatusTextField() {
+    clickOn("#serviceStatus");
+    write("h");
+    verifyThat("#serviceStatus", hasText("h"));
   }
 
   @Test
   public void testResetButton() {
     clickOn("#resetFields");
-    verifyThat("#amountField", hasText(""));
+    verifyThat("#patientName", hasText(""));
+    verifyThat("#rxNum", hasText(""));
+    verifyThat("#roomNum", hasText(""));
+    verifyThat("#assignStaff", hasText(""));
+    verifyThat("#serviceStatus", hasText(""));
   }
 }
