6eedce06286d59d348c9b4423c3a9c1c16713285
Dylan Nguyen <dnguyen2065@gmail.com>
2022-04-03 01:05:43 -0400

    adding submit button disable (also switching to laptop so have to push)

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/LabRequestController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/LabRequestController.java
@@ -2,11 +2,15 @@ package edu.wpi.cs3733.D22.teamX.controllers;
 
 import edu.wpi.cs3733.D22.teamX.App;
 import edu.wpi.cs3733.D22.teamX.Location;
+import edu.wpi.cs3733.D22.teamX.LocationDAO;
+import edu.wpi.cs3733.D22.teamX.LocationDAOImpl;
 import edu.wpi.cs3733.D22.teamX.entity.LabServiceRequest;
-import edu.wpi.cs3733.D22.teamX.entity.MedicalEquipmentServiceRequest;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -14,6 +18,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -28,29 +33,95 @@ public class LabRequestController implements Initializable {
   @FXML private TableColumn<LabServiceRequest, String> service;
   @FXML private TableColumn<LabServiceRequest, String> status;
   @FXML private TableColumn<LabServiceRequest, String> destination;
-  @FXML private TableView<MedicalEquipmentServiceRequest> table;
+  @FXML private TableView<LabServiceRequest> table;
   @FXML private Button ReturnToMain;
-  @FXML private ChoiceBox<String> SelectLab, patientName, assigneeDrop, serviceStatus;
-  LabServiceRequest request;
+  @FXML private Button submitRequest;
+  @FXML
+  private ChoiceBox<String> selectLab, patientName, assigneeDrop, serviceStatus, selectDestination;
+  private LabServiceRequest request;
+  private LocationDAO locationDAO;
+  private List<Location> locations;
 
   @FXML
   public void initialize(URL location, ResourceBundle resources) {
+    locationDAO = new LocationDAOImpl();
+    locations = locationDAO.getAllLocations();
+
+    selectDestination.setItems(this.getLocationNames());
+
+    selectLab.getSelectionModel().select("");
+    patientName.getSelectionModel().select("");
+    assigneeDrop.getSelectionModel().select("");
+    serviceStatus.getSelectionModel().select("");
+    selectDestination.getSelectionModel().select("");
+
+    // FORMATTING----------------------------------------------------
+    serviceStatus.getItems().addAll("", "PROC", "DONE");
+    patientName.getItems().addAll("Patient 1", "Patient 2", "Patient 3", "Patient 4", "Patient 5");
+    assigneeDrop
+        .getItems()
+        .addAll("Doctor 1", "Doctor 2", "Doctor 3", "Nurse 1", "Nurse 2", "Nurse 3");
+    selectLab
+        .getItems()
+        .addAll("Blood Work", "MRI", "Urine Sample", "Stool Sample", "Saliva Sample");
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
     submitCol.setSpacing(20);
     buttonRow.setSpacing(20);
     dropdownCol.setSpacing(20);
     labelCol.setSpacing(28);
-    SelectLab.getItems().addAll("Blood Sample", "Urine Sample", "X-Ray", "CAT Scan", "MRI");
+    // TABLE COLUMN PLACING----------------------------------------------------
+    requestID.setCellValueFactory(new PropertyValueFactory<>("requestID"));
+    patientID.setCellValueFactory(new PropertyValueFactory<>("patientFor"));
+    assigneeTable.setCellValueFactory(new PropertyValueFactory<>("assignee"));
+    service.setCellValueFactory(new PropertyValueFactory<>("service"));
+    status.setCellValueFactory(new PropertyValueFactory<>("status"));
+    destination.setCellValueFactory(new PropertyValueFactory<>("destination"));
+    // table.setItems(labDeliveryList());
   }
+
+  public ObservableList<String> getLocationNames() {
+    ObservableList<String> locationNames = FXCollections.observableArrayList();
+    for (int i = 0; i < locations.size(); i++) {
+      locationNames.add(locations.get(i).getShortName());
+    }
+    return locationNames;
+  }
+
+  @FXML
+  public void checkAllBoxes(){
+    //submitRequest.setDisable(selectLab);
+  }
+
+  // UNCOMMENT WHEN DAO AND DAOIMPL ARE PULLED TO MAIN
+  //  private ObservableList<LabServiceRequest> labDeliveryList() {
+
+  //    ObservableList<LabServiceRequest> labList = FXCollections.observableArrayList();
+  //    LabServiceRequestDAO allLabs = new LabServiceRequestDAOImpl();
+  //    List<LabServiceRequest> inpLabsList =
+  //            allLabs.getAllLabServiceRequests();
+  //    labList.addAll(inpLabsList);
+  //    return labList;
+  //  }
 
   @FXML
   public void submitRequest() {
     request = new LabServiceRequest();
-    request.setRequestID("SAMPLE12");
-    request.setDestination(new Location());
-    // request.setStatus(serviceStatus.getText());
+    request.setRequestID(request.makeRequestID());
+    request.setPatientFor(patientName.getValue());
+    request.setAssignee(assigneeDrop.getValue());
+    request.setService(selectLab.getValue());
+    request.setStatus(serviceStatus.getValue());
+    request.setDestination(locations.get(selectDestination.getSelectionModel().getSelectedIndex()));
+    request.setStatus(serviceStatus.getValue());
+    // UNCOMMENT WHEN DAO AND DAOIMPL ARE PULLED TO MAIN
+    // LabServiceRequestDAO submit = new LabServiceRequestDAOImpl();
+    // submit.addLabServiceRequest(request);
     this.resetFields();
   }
+
+  // private ChoiceBox<String> selectLab, patientName, assigneeDrop, serviceStatus,
+  // selectDestination;
 
   @FXML
   public void ReturnToMain() throws IOException {
@@ -64,6 +135,7 @@ public class LabRequestController implements Initializable {
     patientName.setValue("");
     assigneeDrop.setValue("");
     serviceStatus.setValue("");
-    SelectLab.setValue("");
+    selectLab.setValue("");
+    selectDestination.setValue("");
   }
 }
