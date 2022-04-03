755fa5de0c12dd3e5cd5eff432387ecc309d105d
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-04-03 05:25:42 -0400

    added methods

--- src/main/java/edu/wpi/cs3733/D22/teamX/entity/LabServiceRequestDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/LabServiceRequestDAO.java
@@ -3,4 +3,12 @@ package edu.wpi.cs3733.D22.teamX.entity;
 import java.util.List;
 public interface LabServiceRequestDAO {
     List<LabServiceRequest> getAllLabServiceRequests();
+    
+    LabServiceRequest getLabServiceRequest(String requestID);
+
+    void deleteLabServiceRequest (LabServiceRequest labServiceRequest);
+
+    void updateLabServiceRequest (LabServiceRequest labServiceRequest);
+
+    void addLabServiceRequest (LabServiceRequest labServiceRequest);
 }
