ee7fdf76353a2d0d629bd9603cf923908d652e08
Nadav Konstantine <nadavk2002@gmail.com>
2022-04-03 02:40:14 -0400

    Touched up the spacing and alignment of the textfields and labels to be more aesthetic

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/ReqLaundryController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/ReqLaundryController.java
@@ -4,22 +4,44 @@ import edu.wpi.cs3733.D22.teamX.App;
 import edu.wpi.cs3733.D22.teamX.Location;
 import edu.wpi.cs3733.D22.teamX.entity.LaundyServiceRequest;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class ReqLaundryController {
+public class ReqLaundryController implements Initializable {
+  @FXML private VBox selectionMenuColumn;
+  @FXML private VBox labelColumn;
+  @FXML private VBox submitColumn;
+  @FXML private HBox buttonRow;
   @FXML private Button ToMainMenu;
   @FXML private ChoiceBox<String> selectLaundryType;
   @FXML private TextField roomField, assignStaff, serviceStatus;
 
   @FXML
-  public void initialize() {
+  public void initialize(URL location, ResourceBundle resources) {
+    selectionMenuColumn.setSpacing(20);
+    labelColumn.setSpacing(28);
+    submitColumn.setSpacing(20);
+    buttonRow.setSpacing(20);
     selectLaundryType
         .getItems()
         .addAll(new String[] {"Linens", "Gowns", "Bedding", "Scrubs", "Coats"});
+  }
+
+  @FXML
+  void submitRequest() {
+    LaundyServiceRequest request = new LaundyServiceRequest();
+    request.setRequestID("SAMPLE12");
+    request.setDestination(new Location());
+    request.setStatus(serviceStatus.getText());
+    this.resetFields();
   }
 
   /**
@@ -39,14 +61,5 @@ public class ReqLaundryController {
     selectLaundryType.setValue("");
     assignStaff.setText("");
     serviceStatus.setText("");
-  }
-
-  @FXML
-  void submitRequest() {
-    LaundyServiceRequest request = new LaundyServiceRequest();
-    request.setRequestID("SAMPLE12");
-    request.setDestination(new Location());
-    request.setStatus(serviceStatus.getText());
-    this.resetFields();
   }
 }
