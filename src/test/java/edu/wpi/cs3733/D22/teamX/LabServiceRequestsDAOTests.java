92bf52a27f611ddf4d04ca9fb6b50776202b2c4e
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-04-04 16:18:25 -0400

    Update LabServiceRequestsDAOTests.java

--- src/test/java/edu/wpi/cs3733/D22/teamX/LabServiceRequestsDAOTests.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/LabServiceRequestsDAOTests.java
@@ -1,32 +1,50 @@
 package edu.wpi.cs3733.D22.teamX;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import edu.wpi.cs3733.D22.teamX.entity.MedicalEquipmentServiceRequest;
-import edu.wpi.cs3733.D22.teamX.entity.MedicalEquipmentServiceRequestDAOImpl;
+import edu.wpi.cs3733.D22.teamX.entity.LabServiceRequestDAOImpl;
+import edu.wpi.cs3733.D22.teamX.entity.LocationDAO;
+import edu.wpi.cs3733.D22.teamX.entity.LocationDAOImpl;
 import edu.wpi.cs3733.D22.teamX.exceptions.loadSaveFromCSVException;
-import java.util.NoSuchElementException;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LabServiceRequestsDAOTests {
 
-    //based on CSV file as of 4/4
-    @Before
-    public void setupDB() {
-        // initialize Xdb
-        try {
-            Xdb.initializeDB();
-        } catch (loadSaveFromCSVException e) {
-            e.printStackTrace();
-        }
+  // based on CSV file as of 4/4
+  @Before
+  public void setupDB() {
+    // initialize Xdb
+    try {
+      Xdb.initializeDB();
+    } catch (loadSaveFromCSVException e) {
+      e.printStackTrace();
     }
+  }
 
-    // these test will fail if the csv file changes
-    @Test
-    public void TestDBListRequests() {
-        MedicalEquipmentServiceRequestDAOImpl esrDAOImpl = new MedicalEquipmentServiceRequestDAOImpl();
-        assertEquals(15, esrDAOImpl.getAllMedicalEquipmentServiceRequests().size());
-    }
+  // these test will fail if the csv file changes
+  @Test
+  public void TestDBListRequests() {
+    LabServiceRequestDAOImpl lsrDAOImpl = new LabServiceRequestDAOImpl();
+    assertEquals(15, lsrDAOImpl.getAllLabServiceRequests().size());
+  }
+
+  @Test
+  public void TestDBgetRequest() {
+    LabServiceRequestDAOImpl lsrDAOImpl = new LabServiceRequestDAOImpl();
+    assertEquals(
+        lsrDAOImpl.getLabServiceRequest("LBSR0001"), lsrDAOImpl.getLabServiceRequest("LBSR0001"));
+  }
+
+  @Test
+  public void TestDBgetRequest2() {
+    LabServiceRequestDAOImpl lsrDAOImpl = new LabServiceRequestDAOImpl();
+    LocationDAO locationDAO = new LocationDAOImpl();
+    assertEquals(
+        lsrDAOImpl.getLabServiceRequest("LBSR0001").getDestination(),
+        locationDAO.getLocation("HHALL01203"));
+    assertEquals(lsrDAOImpl.getLabServiceRequest("LBSR0001").getStatus(), "DONE");
+    assertEquals(lsrDAOImpl.getLabServiceRequest("LBSR0001").getAssignee(), "EMPL0001");
+    assertEquals(lsrDAOImpl.getLabServiceRequest("LBSR0001").getService(), "Blood Sample");
+  }
 }
