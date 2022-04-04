38759082ff0de69044d35e68b779e4d3087bd64f
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2022-04-03 22:04:37 -0400

    started csv saver for frontend

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/AppController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/AppController.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.D22.teamX.controllers;
 
 import edu.wpi.cs3733.D22.teamX.App;
 import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -83,6 +84,9 @@ public class AppController {
 
   @FXML
   void ExitApplication() throws IOException {
+//    App.switchScene(
+//        FXMLLoader.load(
+//            getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/CSVFileSaver.fxml")));
     Platform.exit();
   }
 }
--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/BasicLayoutController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/BasicLayoutController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.cs3733.D22.teamX.App;
 import java.io.IOException;
 import java.util.HashMap;
+
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -53,7 +54,10 @@ public class BasicLayoutController {
   }
 
   @FXML
-  void ExitApplication() {
+  void ExitApplication() throws IOException {
+//    App.switchScene(
+//        FXMLLoader.load(
+//            getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/CSVFileSaver.fxml")));
     Platform.exit();
   }
 }
