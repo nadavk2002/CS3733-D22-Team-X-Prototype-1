4a61280ab7da020b7546eff2a0e46d2f2293afc3
Sharonronron <slwu@wpi.edu>
2022-04-04 19:42:58 -0400

    Updated Janitoral Request but from main menu doesn't go to page

--- src/main/java/edu/wpi/cs3733/D22/teamX/entity/JanitorServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/JanitorServiceRequest.java
@@ -4,6 +4,17 @@ package edu.wpi.cs3733.D22.teamX.entity;
 public class JanitorServiceRequest extends ServiceRequest {
   private String serviceType;
 
+  public JanitorServiceRequest() {
+    super();
+    this.serviceType = "";
+  }
+
+  public JanitorServiceRequest(
+      String requestID, Location destination, String status, String assignee, String serviceType) {
+    super(requestID, destination, status, assignee);
+    this.serviceType = serviceType;
+  }
+
   public String getServiceType() {
     return serviceType;
   }
