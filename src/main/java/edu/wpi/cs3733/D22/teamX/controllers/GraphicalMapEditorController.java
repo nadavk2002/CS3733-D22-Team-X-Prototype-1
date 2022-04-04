e7466d99f08926778fe8a96768ae789ec730ea81
NicholasMarkou <nmarkou2000@gmail.com>
2022-04-03 21:45:04 -0400

    Images load when respective button clicked. Locations are red dots on the image

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/GraphicalMapEditorController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/GraphicalMapEditorController.java
@@ -13,9 +13,14 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class GraphicalMapEditorController implements Initializable {
 
@@ -23,11 +28,9 @@ public class GraphicalMapEditorController implements Initializable {
 
   @FXML private HBox hBox1;
 
-  @FXML private TabPane tabPane;
+  @FXML private Group imageGroup;
 
-  @FXML private Tab tabLL2;
-
-  @FXML private Tab tabLL1;
+  @FXML private ImageView imageView;
 
   @FXML private TableView<Location> table;
 
@@ -53,6 +56,81 @@ public class GraphicalMapEditorController implements Initializable {
         FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/app.fxml")));
   }
 
+  @FXML
+  public void LL1Click() {
+    imageGroup.getChildren().clear();
+
+    Image img =
+        new Image(
+            getClass().getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_LL1_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("L1");
+  }
+
+  @FXML
+  public void LL2Click() {
+    imageGroup.getChildren().clear();
+
+    Image img =
+        new Image(
+            getClass().getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_LL2_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("L2");
+  }
+
+  @FXML
+  public void groundClick() {
+    imageGroup.getChildren().clear();
+
+    Image img =
+        new Image(
+            getClass()
+                .getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_Ground_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("G");
+  }
+
+  @FXML
+  public void firstClick() {
+    imageGroup.getChildren().clear();
+
+    Image img =
+        new Image(
+            getClass()
+                .getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_First_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("1");
+  }
+
+  @FXML
+  public void secondClick() {
+    imageGroup.getChildren().clear();
+
+    Image img =
+        new Image(
+            getClass()
+                .getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_Second_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("2");
+  }
+
+  @FXML
+  public void thirdClick() {
+    imageGroup.getChildren().clear();
+    Image img =
+        new Image(
+            getClass()
+                .getResourceAsStream("/edu/wpi/cs3733/D22/teamX/assets/Crop_Third_Floor.png"));
+    ImageView newImage = new ImageView(img);
+    imageGroup.getChildren().add(newImage);
+    drawCircles("3");
+  }
+
   private ObservableList<Location> locationListFill() {
     ObservableList<Location> tableList = FXCollections.observableArrayList();
     LocationDAO allLocations = new LocationDAOImpl();
@@ -63,10 +141,24 @@ public class GraphicalMapEditorController implements Initializable {
     return tableList;
   }
 
+  private void drawCircles(String floor) {
+    LocationDAO allLocations = new LocationDAOImpl();
+    List<Location> locationList = allLocations.getAllLocations();
+    for (int i = 0; i < locationList.size(); i++) {
+      if (locationList.get(i).getFloor().equals(floor)) {
+        Circle circle = new Circle();
+        circle.setRadius(1);
+        circle.setCenterX(locationList.get(i).getxCoord());
+        circle.setCenterY(locationList.get(i).getyCoord());
+        circle.setFill(Paint.valueOf("RED"));
+        imageGroup.getChildren().add(circle);
+      }
+    }
+  }
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    // hBox1 = new HBox(90, tabPane, table);
-    hBox1.getChildren().add(tabPane);
+
     hBox1.getChildren().add(table);
     hBox1.setSpacing(90);
     // Tab tabLL2 = new Tab("Lower Level 2", new ImageView(Crop_LL2_Floor.png));
