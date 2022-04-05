e534a97af454140a1f92790cb39ef2715a17ea35
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-04-04 21:55:43 -0400

    added tests to LabServiceRequestDAOTests

--- src/test/java/edu/wpi/cs3733/D22/teamX/LabServiceRequestsDAOTests.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/LabServiceRequestsDAOTests.java
@@ -1,12 +1,11 @@
 package edu.wpi.cs3733.D22.teamX;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import edu.wpi.cs3733.D22.teamX.entity.LabServiceRequest;
-import edu.wpi.cs3733.D22.teamX.entity.LabServiceRequestDAOImpl;
-import edu.wpi.cs3733.D22.teamX.entity.LocationDAO;
-import edu.wpi.cs3733.D22.teamX.entity.LocationDAOImpl;
+import edu.wpi.cs3733.D22.teamX.entity.*;
 import edu.wpi.cs3733.D22.teamX.exceptions.loadSaveFromCSVException;
+import java.util.NoSuchElementException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -59,5 +58,45 @@ public class LabServiceRequestsDAOTests {
     lsrDAOImpl.updateLabServiceRequest(lsr);
     assertEquals("DONE", lsrDAOImpl.getLabServiceRequest("LBSR0002").getStatus());
   }
-  
+
+  @Test
+  public void testDBLSRequestDelete() {
+    boolean returnVarible = false;
+    LabServiceRequestDAOImpl lsrDAOImpl = new LabServiceRequestDAOImpl();
+    assertEquals(lsrDAOImpl.getLabServiceRequest("LBSR0003").getStatus(), "DONE");
+    // delete
+    LabServiceRequest lsr = lsrDAOImpl.getLabServiceRequest("LBSR0003");
+    lsrDAOImpl.deleteLabServiceRequest(lsr);
+    try {
+      lsrDAOImpl.getLabServiceRequest("LBSR0003");
+    } catch (NoSuchElementException s) {
+      returnVarible = true;
+    }
+    assertTrue(returnVarible);
+  }
+
+  @Test
+  public void testDBLSRequestAdd() {
+    boolean returnVarible = false;
+    LabServiceRequestDAO lsrDAOImpl = new LabServiceRequestDAOImpl();
+    LocationDAO locationDAO = new LocationDAOImpl();
+    try {
+      lsrDAOImpl.getLabServiceRequest("LBSR0016");
+    } catch (NoSuchElementException s) {
+      returnVarible = true;
+    }
+    assertTrue(returnVarible);
+    // add the thing
+    LabServiceRequest lsr =
+        new LabServiceRequest(
+            "LBSR0016",
+            locationDAO.getLocation("FHALL01101"),
+            "DONE",
+            "EMPL0001",
+            "Ultrasound",
+            "PATT0125");
+    // add the thing
+    lsrDAOImpl.addLabServiceRequest(lsr);
+    assertEquals(lsr, lsrDAOImpl.getLabServiceRequest("LBSR0016"));
+  }
 }
