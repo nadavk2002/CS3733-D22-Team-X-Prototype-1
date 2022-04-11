f0f11ad93a2bfb7afa7e75ad83afc94a5aabd1d5
Dylan Nguyen <dnguyen2065@gmail.com>
2022-04-11 13:09:13 -0400

    added search to location table popup

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/GraphicalMapEditorTableOverlayController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/GraphicalMapEditorTableOverlayController.java
@@ -1,8 +1,5 @@
 package edu.wpi.cs3733.D22.teamX.controllers;
 
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import edu.wpi.cs3733.D22.teamX.entity.Location;
 import edu.wpi.cs3733.D22.teamX.entity.LocationDAO;
 import edu.wpi.cs3733.D22.teamX.entity.LocationDAOImpl;
@@ -12,46 +9,85 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class GraphicalMapEditorTableOverlayController implements Initializable {
-  @FXML private JFXTreeTableView<Location> table;
-  @FXML private TreeTableColumn<Location, String> nodeID;
-  @FXML private TreeTableColumn<Location, String> xCoord;
-  @FXML private TreeTableColumn<Location, String> yCoord;
-  @FXML private TreeTableColumn<Location, String> floor;
-  @FXML private TreeTableColumn<Location, String> building;
-  @FXML private TreeTableColumn<Location, String> nodeType;
-  @FXML private TreeTableColumn<Location, String> longName;
-  @FXML private TreeTableColumn<Location, String> shortName;
+  @FXML private TableView<Location> table;
+  @FXML private TableColumn<Location, String> nodeID;
+  @FXML private TableColumn<Location, String> xCoord;
+  @FXML private TableColumn<Location, String> yCoord;
+  @FXML private TableColumn<Location, String> floor;
+  @FXML private TableColumn<Location, String> building;
+  @FXML private TableColumn<Location, String> nodeType;
+  @FXML private TableColumn<Location, String> longName;
+  @FXML private TableColumn<Location, String> shortName;
   private LocationDAO locDAO;
-  //  @FXML TextField searchLocations;
+  @FXML private TextField searchLocationsField;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    searchLocationsField.setPromptText("Search Here");
+
     locDAO = new LocationDAOImpl();
-    table.setColumnResizePolicy(JFXTreeTableView.CONSTRAINED_RESIZE_POLICY);
-    nodeID.setCellValueFactory(new TreeItemPropertyValueFactory<>("nodeID"));
-    xCoord.setCellValueFactory(new TreeItemPropertyValueFactory<>("x"));
-    yCoord.setCellValueFactory(new TreeItemPropertyValueFactory<>("y"));
-    floor.setCellValueFactory(new TreeItemPropertyValueFactory<>("floor"));
-    building.setCellValueFactory(new TreeItemPropertyValueFactory<>("building"));
-    nodeType.setCellValueFactory(new TreeItemPropertyValueFactory<>("nodeType"));
-    longName.setCellValueFactory(new TreeItemPropertyValueFactory<>("longName"));
-    shortName.setCellValueFactory(new TreeItemPropertyValueFactory<>("shortName"));
-    locationListFill();
+    table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    nodeID.setCellValueFactory(new PropertyValueFactory<>("nodeID"));
+    xCoord.setCellValueFactory(new PropertyValueFactory<>("x"));
+    yCoord.setCellValueFactory(new PropertyValueFactory<>("y"));
+    floor.setCellValueFactory(new PropertyValueFactory<>("floor"));
+    building.setCellValueFactory(new PropertyValueFactory<>("building"));
+    nodeType.setCellValueFactory(new PropertyValueFactory<>("nodeType"));
+    longName.setCellValueFactory(new PropertyValueFactory<>("longName"));
+    shortName.setCellValueFactory(new PropertyValueFactory<>("shortName"));
+    searchLocations();
   }
 
-  private void locationListFill() {
+  void searchLocations() {
+    FilteredList<Location> filteredData = new FilteredList<>(locationListFill(), b -> true);
+    searchLocationsField
+        .textProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              filteredData.setPredicate(
+                  location -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                      return true;
+                    }
+                    String lowerCaseFilter = newValue.toLowerCase();
+                    if (location.getNodeID().toLowerCase().contains(lowerCaseFilter)) {
+                      return true;
+                    } else if (location.getBuilding().toLowerCase().contains(lowerCaseFilter)) {
+                      return true;
+                    } else if (location.getNodeType().toLowerCase().contains(lowerCaseFilter)) {
+                      return true;
+                    } else if (location.getLongName().toLowerCase().contains(lowerCaseFilter)) {
+                      return true;
+                    } else if (location.getShortName().toLowerCase().contains(lowerCaseFilter)) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  });
+            });
+    SortedList<Location> sortedData = new SortedList<>(filteredData);
+    sortedData.comparatorProperty().bind(table.comparatorProperty());
+    populateTable(sortedData);
+  }
+
+  private ObservableList<Location> locationListFill() {
     ObservableList<Location> tableList = FXCollections.observableArrayList();
     locDAO = new LocationDAOImpl();
     List<Location> locationList = locDAO.getAllLocations();
     tableList.addAll(locationList);
-    TreeItem<Location> root = new RecursiveTreeItem<>(tableList, RecursiveTreeObject::getChildren);
-    table.setRoot(root);
+    return tableList;
+  }
+
+  private void populateTable(ObservableList<Location> locations) {
+    table.setItems(locations);
   }
 }
