4ecc71e26aa6767679a6abd5d7efbd1d230fcc08
NicholasMarkou <nmarkou2000@gmail.com>
2022-04-12 18:25:10 -0400

    Table complete

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/ServiceRequestTableController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/ServiceRequestTableController.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,20 +17,39 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 public class EquipReqTableController implements Initializable {
   @FXML private TableView<ServiceRequest> table;
-  // @FXML private TableColumn<MedicalEquipmentServiceRequest, String> requester;
   @FXML private TableColumn<ServiceRequest, String> assignee; // spike c, Quantity
   @FXML private TableColumn<ServiceRequest, String> requestStatus; // spike c, Status
-  @FXML private TableColumn<ServiceRequest, Location> destination; // spike c destination
+  @FXML private TableColumn<ServiceRequest, String> destination; // spike c destination
   @FXML private TableColumn<ServiceRequest, String> requestID; // spike c RequestID
+  @FXML private TableColumn<ServiceRequest, String> serviceType;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     requestStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
-    destination.setCellValueFactory(new PropertyValueFactory<>("destination"));
+    destination.setCellValueFactory(
+        new Callback<
+            TableColumn.CellDataFeatures<ServiceRequest, String>, ObservableValue<String>>() {
+          @Override
+          public ObservableValue<String> call(
+              TableColumn.CellDataFeatures<ServiceRequest, String> param) {
+            return new SimpleStringProperty(param.getValue().getDestination().getLongName());
+          }
+        });
+    serviceType.setCellValueFactory(
+        new Callback<
+            TableColumn.CellDataFeatures<ServiceRequest, String>, ObservableValue<String>>() {
+          @Override
+          public ObservableValue<String> call(
+              TableColumn.CellDataFeatures<ServiceRequest, String> param) {
+            return new SimpleStringProperty(param.getValue().getClass().getSimpleName());
+          }
+        });
+
     requestID.setCellValueFactory(new PropertyValueFactory<>("requestID"));
     assignee.setCellValueFactory(new PropertyValueFactory<>("assignee"));
     table.setItems(FXCollections.observableList(listOfRequests()));
