43b722d2fa360197e93c2a0a12d5ba811aaa7fc1
salochinYom <73899470+salochinYom@users.noreply.github.com>
2022-04-12 16:28:58 -0400

    added methods required by DAO Interface,

--- src/main/java/edu/wpi/cs3733/D22/teamX/entity/MealServiceRequestDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/MealServiceRequestDAO.java
@@ -1,4 +1,55 @@
 package edu.wpi.cs3733.D22.teamX.entity;
 
-public class MealServiceRequestDAO {
+import java.util.List;
+
+public class MealServiceRequestDAO implements DAO<MealServiceRequest> {
+    @Override
+    public List<MealServiceRequest> getAllRecords() {
+        return null;
+    }
+
+    @Override
+    public MealServiceRequest getRecord(String recordID) {
+        return null;
+    }
+
+    @Override
+    public void deleteRecord(MealServiceRequest recordObject) {
+
+    }
+
+    @Override
+    public void updateRecord(MealServiceRequest recordObject) {
+
+    }
+
+    @Override
+    public void addRecord(MealServiceRequest recordObject) {
+
+    }
+
+    @Override
+    public void createTable() {
+
+    }
+
+    @Override
+    public void dropTable() {
+
+    }
+
+    @Override
+    public boolean loadCSV() {
+        return false;
+    }
+
+    @Override
+    public boolean saveCSV(String dirPath) {
+        return false;
+    }
+
+    @Override
+    public String makeID() {
+        return null;
+    }
 }
