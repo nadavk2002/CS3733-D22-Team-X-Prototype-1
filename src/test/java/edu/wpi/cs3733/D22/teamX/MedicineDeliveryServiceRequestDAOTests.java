531b9f41f22c1b945df1a283c57720957a6796f3
salochinYom <nrmoy2@gmail.com>
2022-04-12 23:46:13 -0400

    changed csv to valid locations

--- src/test/java/edu/wpi/cs3733/D22/teamX/MedicineDeliveryServiceRequestDAOTests.java
+++ src/test/java/edu/wpi/cs3733/D22/teamX/MedicineDeliveryServiceRequestDAOTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import edu.wpi.cs3733.D22.teamX.entity.MedicineDeliverServiceRequestDAO;
 import edu.wpi.cs3733.D22.teamX.exceptions.loadSaveFromCSVException;
 import java.awt.*;
+import java.util.NoSuchElementException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,5 +26,19 @@ public class MedicineDeliveryServiceRequestDAOTests {
   @Test
   public void TestGetRecord() {
     assertTrue(DAO.getRecord("MDSR0001").getStatus().equals("PROC"));
+  }
+
+  @Test
+  public void TestdeleteServiceRecord() {
+    Boolean testReturn = false; // must be changed to true
+    assertTrue(DAO.getRecord("PMSR0008").getStatus().equals("DONE")); // make sure the thing exists
+    // delete the record
+    DAO.deleteRecord(DAO.getRecord("PMSR0008"));
+    try {
+      DAO.getRecord("PMSR0008");
+    } catch (NoSuchElementException e) {
+      testReturn = true;
+    }
+    assertTrue(testReturn);
   }
 }
