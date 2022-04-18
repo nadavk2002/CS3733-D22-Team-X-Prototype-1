1077371f2029b2ef8953991a8471da653bf4fb12
salochinYom <nrmoy2@gmail.com>
2022-04-18 15:05:57 -0400

    updated fxml to point at controller that exists

---
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/SharpsDisposalRequestController.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs3733.D22.teamX.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SharpsDisposalRequestController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
