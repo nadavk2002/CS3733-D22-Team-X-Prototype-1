81265a20bf450e87b44dc05d6e7052d6c7effebe
salochinYom <nrmoy2@gmail.com>
2022-04-19 17:13:47 -0400

    changed add and update buttons to only be enabled when text fields change

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/EmployeeViewerController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/EmployeeViewerController.java
@@ -5,6 +5,7 @@ import edu.wpi.cs3733.D22.teamX.entity.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -43,6 +44,40 @@ public class EmployeeViewerController implements Initializable {
     EmployeeID.setItems(getEmployeeIDs());
     // setup on action
     EmployeeID.setOnAction((ActionEvent event) -> updateFields());
+    //    firstName.setOnAction((ActionEvent event) -> enableAddUpdate());
+    //    lastName.setOnAction((ActionEvent event) -> enableAddUpdate());
+    //    clearanceType.setOnAction((ActionEvent event) -> enableAddUpdate());
+    //    jobTitle.setOnAction((ActionEvent event) -> enableAddUpdate());
+  }
+
+  @FXML
+  private void enableAddUpdate() {
+    boolean isDisabled =
+        firstName.getText().equals("")
+            || lastName.getText().equals("")
+            || clearanceType.getText().equals("")
+            || jobTitle.getText().equals("");
+    try {
+      isDisabled =
+          firstName.getText().equals(employeeDAO.getRecord(EmployeeID.getValue()).getFirstName())
+                  && lastName
+                      .getText()
+                      .equals(employeeDAO.getRecord(EmployeeID.getValue()).getLastName())
+                  && clearanceType
+                      .getText()
+                      .equals(employeeDAO.getRecord(EmployeeID.getValue()).getClearanceType())
+                  && jobTitle
+                      .getText()
+                      .equals(employeeDAO.getRecord(EmployeeID.getValue()).getJobTitle())
+              || firstName.getText().equals("")
+              || lastName.getText().equals("")
+              || clearanceType.getText().equals("")
+              || jobTitle.getText().equals("");
+    } catch (NoSuchElementException e) {
+    }
+
+    addNew.setDisable(isDisabled);
+    update.setDisable(isDisabled);
   }
 
   private ObservableList<String> getEmployeeIDs() {
@@ -72,6 +107,9 @@ public class EmployeeViewerController implements Initializable {
           new PropertyValueFactory<ServiceRequest, String>("LocationShortName"));
       tableStatus.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("Status"));
       table.setItems(FXCollections.observableList(SRDAOToOL()));
+
+      // reset the buttons
+      enableAddUpdate();
     }
   }
 
@@ -109,6 +147,10 @@ public class EmployeeViewerController implements Initializable {
 
     // clearChoiceBox
     EmployeeID.setValue("");
+
+    // disable add and update buttons
+    addNew.setDisable(true);
+    update.setDisable(true);
 
     // enable choicebox updates
     allowUpdateFields = true;
