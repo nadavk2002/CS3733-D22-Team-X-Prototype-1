8e99bc34924c9b2e748df028bbd1395221e34b8d
salochinYom <nrmoy2@gmail.com>
2022-04-26 19:42:04 -0400

    updated maintmence controller

--- src/main/java/edu/wpi/cs3733/D22/teamX/entity/MaintenanceServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/MaintenanceServiceRequest.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D22.teamX.entity;
 
+import java.time.LocalDateTime;
+
 public class MaintenanceServiceRequest extends ServiceRequest {
   private String description;
 
@@ -13,7 +15,20 @@ public class MaintenanceServiceRequest extends ServiceRequest {
       Location destination,
       String status,
       Employee assignee,
+      LocalDateTime creationTime,
+      LocalDateTime PROCTime,
+      LocalDateTime DONETime,
       String description) {
+    super(requestID, destination, status, assignee, creationTime, PROCTime, DONETime);
+    this.description = description;
+  }
+
+  public MaintenanceServiceRequest(
+          String requestID,
+          Location destination,
+          String status,
+          Employee assignee,
+          String description) {
     super(requestID, destination, status, assignee);
     this.description = description;
   }
