18b07930b8943b43b5926ba87bb7aa250964d1df
norbertax28 <norbertax28@gmail.com>
2022-04-25 21:07:46 -0400

    Added Covid Testing Page

--- src/main/java/edu/wpi/cs3733/D22/teamX/controllers/CovidPageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/CovidPageController.java
@@ -1,10 +1,22 @@
 package edu.wpi.cs3733.D22.teamX.controllers;
 
+import edu.wpi.cs3733.D22.teamX.App;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
 public class CovidPageController implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
+
+  @FXML
+  public void switchTesting() throws IOException {
+    App.switchScene(
+        FXMLLoader.load(
+            getClass().getResource("/edu/wpi/cs3733/D22/teamX/views/CovidTestingPage.fxml")));
+    CSVFileSaverController.loaded = false;
+  }
 }
