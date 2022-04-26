f86ec6a9950ca3225669118f1ac2231c97c1fd8e
salochinYom <nrmoy2@gmail.com>
2022-04-26 19:17:51 -0400

    updated laundy req constructor to add time stamps

--- src/main/java/edu/wpi/cs3733/D22/teamX/entity/LaundyServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamX/entity/LaundyServiceRequest.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D22.teamX.entity;
 
+import java.time.LocalDateTime;
+
 /** Represents a Laundry Service Request */
 public class LaundyServiceRequest extends ServiceRequest {
   private String service;
@@ -14,6 +16,15 @@ public class LaundyServiceRequest extends ServiceRequest {
     super(requestID, destination, status, assignee);
     this.service = service;
   }
+  public LaundyServiceRequest(
+          String requestID, Location destination, String status, Employee assignee,
+          LocalDateTime creationTime,
+          LocalDateTime PROCTime,
+          LocalDateTime DONETime, String service) {
+    super(requestID, destination, status, assignee, creationTime, PROCTime, DONETime);
+    this.service = service;
+  }
+
 
   public String getService() {
     return service;
