9c462d05dbc6c0c4fd2e44ebb6dea3607ac9c8cb
norbertax28 <norbertax28@gmail.com>
2022-04-26 21:36:00 -0400

    Added part of the credit page

---
+++ src/main/java/edu/wpi/cs3733/D22/teamX/controllers/CreditsPageController.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs3733.D22.teamX.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreditsPageController implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
